4e342feec982b9e9ce68e9552ca9bc3a3eb6a969
ImaneBentaleb <imanebentaleb2030@gmail.com>
2025-02-02 17:53:54 +0100

    changed cors link

--- server/server.js
+++ server/server.js
@@ -3,11 +3,18 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+/* app.use(cors()); */
+app.use(cors({
+  origin: 'http://bentaleb.rf.gd', // Replace with your actual frontend URL
+  methods: 'POST',
+  allowedHeaders: 'Content-Type'
+}));
+
 app.use(bodyParser.json());
 
 // Test Route for Root
